7cb6a896f1be3d8f01e4158fa21a440bb996e0c9
Islomjonsdev <islomjonmukhammadaliev4@gmail.com>
2025-03-11 17:22:03 +0500

    fixed

--- src/components/Shared/Hero/Hero.jsx
+++ src/components/Shared/Hero/Hero.jsx
@@ -1,17 +1,50 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import { Scrollbar } from "swiper/modules";
 
 const Hero = () => {
+  const heroData = [
+    {
+      title: "  Make a Difference Today!",
+      text: "Explore volunteering activities that match your interests and skills.",
+    },
+    {
+      title: "  Make a Difference Today!",
+      text: "Explore volunteering activities that match your interests and skills.",
+    },
+    {
+      title: "  Make a Difference Today!",
+      text: "Explore volunteering activities that match your interests and skills.",
+    },
+    {
+      title: "  Make a Difference Today!",
+      text: "Explore volunteering activities that match your interests and skills.",
+    },
+  ];
   return (
     <section className="bg-white shadow-sm shadow-gray-300">
       <div className="max-w-[1336px] w-full mx-auto px-[20px]">
-        <div className="flex flex-col items-center py-[120px]">
-          <h2 className="text-black font-bold text-[60px] leading-[100%] mb-[40px]">
-            Make a Difference Today!
-          </h2>
-          <p className="text-black font-normal text-4 leading-[27px]">
-            Explore volunteering activities that match your interests and
-            skills.
-          </p>
+        <div className="text-center py-[120px]">
+          <Swiper
+            // scrollbar={{
+            //   hide: true,
+            // }}
+            modules={[Scrollbar]}
+            className="mySwiper"
+          >
+            {heroData?.map((item) => (
+              <SwiperSlide key={item?.title}>
+                <h2 className="text-black font-bold text-[60px] leading-[100%] mb-[40px]">
+                  {item?.title}
+                </h2>
+                <p className="text-black font-normal text-4 leading-[27px]">
+                  {item?.text}
+                </p>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </section>
