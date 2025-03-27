0cb532220d3ac839f3e62902d8e0c119b1a99d2a
Islomjonsdev <islomjonmukhammadaliev4@gmail.com>
2025-03-27 17:22:01 +0500

    fixed

--- src/api/apiRoot.js
+++ src/api/apiRoot.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRoot = axios.create({
-    baseURL: "https://vacansy-production.up.railway.app",
-    timeout: 10000,
-})
+  baseURL: "https://vacansy.onrender.com",
+  timeout: 10000,
+});
 
-export { apiRoot }
+export { apiRoot };
