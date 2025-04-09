b776f8a8b024ddbd56824be9c53736cfc88cef49
Islomjonsdev <islomjonmukhammadaliev4@gmail.com>
2025-04-09 22:59:28 +0500

    post done education

--- src/components/Shared/Education/Education.jsx
+++ src/components/Shared/Education/Education.jsx
@@ -6,9 +6,11 @@ import { useEffect } from "react";
 import { apiRoot } from "../../../api/apiRoot";
 import { IoAddOutline } from "react-icons/io5";
 import { MdEdit, MdDelete } from "react-icons/md";
+import SkillModal from "../../ui/Modal/skill-modal";
 
 const Education = () => {
   const [showMore, setShowMore] = useState(false);
+  const [openEducation, setOpenEducation] = useState(false);
   const token = localStorage.getItem("accessToken");
   const [education, setEducation] = useState([]);
   useEffect(() => {
@@ -39,60 +41,69 @@ const Education = () => {
   };
 
   return (
-    <section className="bg-white shadow-sm shadow-gray-300 p-6 mb-4">
-      <div className="mb-6 flex items-center justify-between">
-        <Title title={"Education"} />
-        <button>
-          <IoAddOutline className="w-6 h-6" />
-        </button>
-      </div>
-      <div>
-        {education?.map(
-          ({
-            description,
-            id,
-            field_of_study,
-            institution,
-            start_date,
-            end_date,
-          }) => (
-            <div className="mb-6 flex items-center justify-between" key={id}>
-              <div>
-                <h3 className="font-inter font-normal text-black leading-[32px] text-[24px] mb-2">
-                  {field_of_study}
-                </h3>
-                <div className="flex items-center gap-2 mb-2">
-                  <span className="text-[#1D1C1C] font-inter font-normal text-4 leading-6">
-                    {institution}
-                  </span>
-                  <p className="text-[#313D44] font-inter font-normal text-[12px] leading-4">
-                    {`${formatDate(start_date)} - ${formatDate(end_date)}`}
+    <>
+      <section className="bg-white shadow-sm shadow-gray-300 p-6 mb-4">
+        <div className="mb-6 flex items-center justify-between">
+          <Title title={"Education"} />
+          <button onClick={() => setOpenEducation(true)}>
+            <IoAddOutline className="w-6 h-6" />
+          </button>
+        </div>
+        <div>
+          {education?.map(
+            ({
+              description,
+              id,
+              field_of_study,
+              institution,
+              start_date,
+              end_date,
+            }) => (
+              <div className="mb-6 flex items-center justify-between" key={id}>
+                <div>
+                  <h3 className="font-inter font-normal text-black leading-[32px] text-[24px] mb-2">
+                    {field_of_study}
+                  </h3>
+                  <div className="flex items-center gap-2 mb-2">
+                    <span className="text-[#1D1C1C] font-inter font-normal text-4 leading-6">
+                      {institution}
+                    </span>
+                    <p className="text-[#313D44] font-inter font-normal text-[12px] leading-4">
+                      {`${formatDate(start_date)} - ${formatDate(end_date)}`}
+                    </p>
+                  </div>
+                  <p className="text-black font-inter font-normal text-[12px] leading-[100%]">
+                    {description}
                   </p>
                 </div>
-                <p className="text-black font-inter font-normal text-[12px] leading-[100%]">
-                  {description}
-                </p>
-              </div>
 
-              <div className="flex items-center gap-2">
-                <button>
-                  <MdEdit className="w-4 h-4" />
-                </button>
-                <button>
-                  <MdDelete className="w-4 h-4" />
-                </button>
+                <div className="flex items-center gap-2">
+                  <button>
+                    <MdEdit className="w-4 h-4" />
+                  </button>
+                  <button>
+                    <MdDelete className="w-4 h-4" />
+                  </button>
+                </div>
               </div>
-            </div>
-          )
-        )}
-      </div>
-      <div className="flex items-center justify-between cursor-pointer">
-        <p className="text-[#313D44] font-inter font-normal text-[16px] leading-[24px]">
-          {showMore ? "Show less" : "Show more"}
-        </p>
-        <img src={rightIcon} alt="rightIcon" width={28} height={16} />
-      </div>
-    </section>
+            )
+          )}
+        </div>
+        <div className="flex items-center justify-between cursor-pointer">
+          <p className="text-[#313D44] font-inter font-normal text-[16px] leading-[24px]">
+            {showMore ? "Show less" : "Show more"}
+          </p>
+          <img src={rightIcon} alt="rightIcon" width={28} height={16} />
+        </div>
+      </section>
+      {openEducation && (
+        <SkillModal close={() => setOpenEducation(false)}>
+          <div className="bg-white rounded-[8px] p-4">
+            <h1 className="mb-3 font-semibold text-[25px]">Add education</h1>
+          </div>
+        </SkillModal>
+      )}
+    </>
   );
 };
 
